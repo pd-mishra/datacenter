05ef5a4cef9a3f66a6049f36533ed0e74bf30eb6
pd-mishra <pd.mishra@gmail.com>
2021-02-08 14:59:19 +0530

    create metric controller

---
+++ src/app/metric/metric.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-metric',
+  templateUrl: './metric.component.html',
+  styleUrls: ['./metric.component.css']
+})
+export class MetricComponent implements OnInit {
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input('used') value: number = 0;
+  @Input('available') max: number = 100;
+  
+  isDanger() {
+    return this.value / this.max > 0.7;
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
